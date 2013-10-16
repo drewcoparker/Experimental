a324e7fdb54f51cc9eef72dd6048076449410560
drewcoparker <drew.i.parker@gmail.com>
2013-10-16 10:16:17 -0600

    Sorting out structure
    Added the SketchupFlags class to project.

---
+++ sketchup_flags.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/env ruby
+
+
+class SketchupFlags
+
+  def maccmdflags(cmd, flag1, flag2, flag3)
+
+  @cmd = cmd
+  @flag1 = flag1
+  @flag2 = flag2
+  @flag3 = flag3
+
+  end
+
+  def wincmdflags(cmd, flag1, flag2, flag3)
+
+  @cmd = cmd
+  @flag1 = flag1
+  @flag2 = flag2
+  @flag3 = flag3
+
+  end
+
+end
